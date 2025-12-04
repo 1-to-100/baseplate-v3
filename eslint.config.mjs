7bbb4cc57ebc45f2359ec5a19b1e9fa10e2aa755
Ruslan Zhelezko <ruszh93@gmail.com>
2025-12-04 13:52:44 +0200

    refactor: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from "eslint-plugin-storybook";
+import prettier from "eslint-plugin-prettier";
 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -12,18 +13,31 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript"), {
-  rules: {
-    "@typescript-eslint/no-unused-vars": "off",
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+    },
   },
-}, {
-  ignores: [
-    "node_modules/**",
-    ".next/**",
-    "build/**",
-    "dist/**",
-    "src/lib/supabase/server.ts",
-  ],
-}, ...storybook.configs["flat/recommended"]];
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "build/**",
+      "dist/**",
+      "src/lib/supabase/server.ts",
+    ],
+  },
+  ...storybook.configs["flat/recommended"],
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+];
 
 export default eslintConfig;
