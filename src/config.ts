e172332ea9e6ade8cd11412e2b9d80e7685761ac
Bogdan Gusak <bogdan@huboxt.com>
2025-12-23 16:11:20 +0200

    Added back some cognito and auth0 values. Investigating the reason of them to be here

--- src/config.ts
+++ src/config.ts
@@ -11,10 +11,21 @@ export interface Config {
     primaryColor: PrimaryColor;
     themeColor: string;
     url: string;
+    version?: string;
   };
   logLevel: keyof typeof LogLevel;
   auth: { strategy: keyof typeof AuthStrategy };
+  auth0: {
+    secret?: string;
+    baseUrl?: string;
+    issuerBaseUrl?: string;
+    clientId?: string;
+    clientSecret?: string;
+  };
+  cognito: { identityPoolId?: string; userPoolClientId?: string; userPoolId?: string };
   supabase: { url?: string; anonKey?: string };
+  mapbox: { apiKey?: string };
+  gtm?: { id?: string };
 }
 
 export const config = {
@@ -25,8 +36,9 @@ export const config = {
     themeColor: '#090a0b',
     primaryColor: 'palatinateBlue',
     url: getSiteURL(),
+    version: process.env.NEXT_PUBLIC_SITE_VERSION,
   },
-  logLevel: LogLevel.ALL,
+  logLevel: (process.env.NEXT_PUBLIC_LOG_LEVEL as keyof typeof LogLevel) || LogLevel.ALL,
   auth: {
     strategy: (() => {
       const envStrategy = process.env.NEXT_PUBLIC_AUTH_STRATEGY;
@@ -37,8 +49,22 @@ export const config = {
       return AuthStrategy.CUSTOM;
     })(),
   },
+  auth0: {
+    secret: process.env.AUTH0_SECRET,
+    baseUrl: process.env.AUTH0_BASE_URL,
+    issuerBaseUrl: process.env.AUTH0_ISSUER_BASE_URL,
+    clientId: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  },
+  cognito: {
+    identityPoolId: process.env.NEXT_PUBLIC_COGNITO_IDENTITY_POOL_ID,
+    userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID,
+    userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID,
+  },
   supabase: {
     url: process.env.NEXT_PUBLIC_SUPABASE_URL,
     anonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
+  mapbox: { apiKey: process.env.NEXT_PUBLIC_MAPBOX_API_KEY },
+  gtm: { id: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID },
 } satisfies Config;
